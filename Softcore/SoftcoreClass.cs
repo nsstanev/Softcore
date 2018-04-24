497e4736e7117a6d9db43afe9d74626d3355b62a
Nikolay Stanev <N.stanev@synergysportstech.com>
2018-04-24 11:09:22 +0300

    Usage of hardcore

--- Softcore/SoftcoreClass.cs
+++ Softcore/SoftcoreClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hardcore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Softcore
     {
         public void DoSomething()
         {
+            var hc = new HardcoreClass();
         }
     }
 }
